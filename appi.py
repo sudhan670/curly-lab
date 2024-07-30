87b69b1b934a48531e642e516e2ff1c420540676
SUDHAN E D <88775663+sudhan670@users.noreply.github.com>
2024-07-30 10:47:48 +0530

    Update appi.py

--- appi.py
+++ appi.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
